97736ad44697bcc2335f2b21b9ea1b3535791a43
matthewcluck <matthewacluck@gmail.com>
2023-02-03 22:03:45 -0600

    Initial layout made. Global variables created

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let place = document.querySelector('#place');
+let temperature = document.querySelector('#temperature');
+let weatherType = document.querySelector('#weathertype');
+let wind = document.querySelector('#wind');
+let humidity = document.querySelector('#humidity');
+let findLabel = document.querySelector('#findlabel');
+
 async function getWeather() {
     let location = 'London';
     let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=d674a55ecc44f0d1f03536de861f665c`);
@@ -6,3 +13,9 @@ async function getWeather() {
 }
 
 getWeather();
+
+place.innerHTML = 'London,UK';
+temperature.innerHTML = '75°F';
+weatherType.innerHTML = 'Rainy';
+wind.innerHTML = 'Wind 10 mph NE';
+humidity.innerHTML = 'Humidity 40%';
