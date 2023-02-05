4a98c74e7a28a281da67ac212af0615bab729a0c
matthewcluck <matthewacluck@gmail.com>
2023-02-04 19:05:31 -0600

    Basic weather app completed

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ let temperature = document.querySelector('#temperature');
 let weatherType = document.querySelector('#weathertype');
 let wind = document.querySelector('#wind');
 let humidity = document.querySelector('#humidity');
+let find = document.querySelector('#find');
 let findLabel = document.querySelector('#findlabel');
 
-async function getWeather() {
-    let location = 'London';
+async function getWeather(location) {
     let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=d674a55ecc44f0d1f03536de861f665c`);
     let wxData = await url.json();
+
+    place.innerHTML = location + " " + wxData.sys.country;
+    let fahrenheit = Math.round((((wxData.main.temp - 273.15)*9)/5) + 32);
+    let windMph = Math.round(wxData.wind.speed*2.37);
+    temperature.innerHTML = `${fahrenheit}°F`;
+    weatherType.innerHTML = `${wxData.weather[0].description}`;
+    wind.innerHTML = `Wind ${windMph} mph`;
+    humidity.innerHTML = `Humidity ${wxData.main.humidity}%`;
     console.log(wxData);
 }
 
-getWeather();
+let locationA = 'Arlington';
+getWeather(locationA);
 
-place.innerHTML = 'London,UK';
-temperature.innerHTML = '75°F';
-weatherType.innerHTML = 'Rainy';
-wind.innerHTML = 'Wind 10 mph NE';
-humidity.innerHTML = 'Humidity 40%';
+find.addEventListener('search', () => {
+    let locationB = find.value;
+    getWeather(locationB);
+})
